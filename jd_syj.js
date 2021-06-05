982dcaba361c7010141c1e43d88fdbedcae2530a
danwangshimoluo <evislcq@foxmail.com>
2021-06-05 22:05:02 +0800

    update

--- jd_syj.js
+++ jd_syj.js
@@ -1,11 +1,6 @@
 /*
- * @Author: lxk0301 https://gitee.com/lxk0301
- * @Date: 2020-11-27 09:19:21
- * @Last Modified by: lxk0301
- * @Last Modified time: 2021-5-21 17:58:02
- */
-/*
 赚京豆脚本，一：做任务 天天领京豆(加速领京豆)、三：赚京豆-瓜分京豆
+Last Modified time: 2021-5-21 17:58:02
 活动入口：赚京豆(微信小程序)-赚京豆-签到领京豆
 更新地址：https://gitee.com/lxk0301/jd_scripts/raw/master/jd_syj.js
 已支持IOS双京东账号, Node.js支持N个京东账号
@@ -51,7 +46,7 @@ const JD_API_HOST = 'https://api.m.jd.com/api';
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
     return;
   }
-  await getAuthorShareCode('https://a.nz.lu/jd_zz.json');
+  await getAuthorShareCode('http://cdn.annnibb.me/jd_zz.json');
   await getAuthorShareCode('https://raw.githubusercontent.com/gitupdate/updateTeam/master/shareCodes/jd_zz.json');
   await getRandomCode();
   for (let i = 0; i < cookiesArr.length; i++) {
