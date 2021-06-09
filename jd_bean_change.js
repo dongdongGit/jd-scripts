6eea7c1900ebfb2eefc34ec9756d5d2b09148cbc
danwangshimoluo <evislcq@foxmail.com>
2021-06-09 19:28:00 +0800

    update

--- jd_bean_change.js
+++ jd_bean_change.js
@@ -1,11 +1,6 @@
 /*
- * @Author: lxk0301 https://gitee.com/lxk0301
- * @Date: 2020-11-01 16:25:41
- * @Last Modified by:   lxk0301
- * @Last Modified time: 2021-05-17 15:25:41
- */
-/*
 京东资产变动通知脚本：https://gitee.com/lxk0301/jd_scripts/raw/master/jd_bean_change.js
+Modified time: 2021-06-9 15:25:41
 统计昨日京豆的变化情况，包括收入，支出，以及显示当前京豆数量,目前小问题:下单使用京豆后,退款重新购买,计算统计会出现异常
 统计红包以及过期红包
 网页查看地址 : https://bean.m.jd.com/beanDetail/index.action?resourceValue=bean
@@ -21,7 +16,7 @@ cron "2 9 * * *" script-path=https://gitee.com/lxk0301/jd_scripts/raw/master/jd_
 =============Surge===========
 [Script]
 京东资产变动通知 = type=cron,cronexp="2 9 * * *",wake-system=1,timeout=3600,script-path=https://gitee.com/lxk0301/jd_scripts/raw/master/jd_bean_change.js
-   
+
 ============小火箭=========
 京东资产变动通知 = type=cron,script-path=https://gitee.com/lxk0301/jd_scripts/raw/master/jd_bean_change.js, cronexpr="2 9 * * *", timeout=3600, enable=true
  */
@@ -307,7 +302,7 @@ function redPacket() {
             t.setHours(0, 0, 0, 0)
             t = parseInt((t - 1) / 1000)
             for (let vo of data.useRedInfo.redList || []) {
-              if (vo.activityName.includes("京喜")) {
+              if (vo.orgLimitStr && vo.orgLimitStr.includes("京喜")) {
                 $.jxRed += parseFloat(vo.balance)
                 if (vo['endTime'] === t) {
                   $.jxRedExpire += parseFloat(vo.balance)
