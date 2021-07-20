85ecd4b337ef0d7e55389bc5e927c12326034789
dongdongGit <1697919363@qq.com>
2021-07-20 22:45:52 +0800

    update JDJRValidator path

--- utils/JDJRValidator_Pure.js
+++ utils/JDJRValidator_Pure.js
@@ -4,7 +4,7 @@ const stream = require('stream');
 const zlib = require('zlib');
 const vm = require('vm');
 const PNG = require('png-js');
-let UA = require('./USER_AGENTS.js').USER_AGENT;
+let UA = require('../USER_AGENTS.js').USER_AGENT;
 const validatorCount = process.env.JDJR_validator_Count ? process.env.JDJR_validator_Count : 100
 
 Math.avg = function average() {
