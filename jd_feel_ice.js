8c5df76c0b5810712f5f030ad865a9606341f185
dongdong <1697919363@qq.com>
2021-07-23 09:10:59 +0800

    update jd_feel_ice commit

--- jd_feel_ice.js
+++ jd_feel_ice.js
@@ -4,7 +4,7 @@ https://xinrui2-isv.isvjcloud.com/jd-tourism/load_app/load_app.html
 更新地址：https://raw.githubusercontent.com/Wenmoux/scripts/wen/jd/jd_ddnc_farmpark.js
 ============Quantumultx===============
 [task_local]
-# 全民摸冰
+# 入口：京东家电-清凉一夏-摸冰领补贴
 6 9,12 * * * https://raw.githubusercontent.com/Wenmoux/scripts/wen/jd/jd_mb.js, tag=全民摸冰, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
 */
 const $ = new Env('全民摸冰');
