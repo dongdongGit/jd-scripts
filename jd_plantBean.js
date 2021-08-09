85a52466fe191c140f45cce349bc26370326fd71
dongdongGit <1697919363@qq.com>
2021-08-09 22:51:39 +0800

    fix jd_plantBean

--- jd_plantBean.js
+++ jd_plantBean.js
@@ -1,5 +1,5 @@
 /*
-种豆得豆 脚本更新地址：https://gitee.com/lxk0301/jd_scripts/raw/master/jd_plantBean.js
+种豆得豆 脚本更新地址：jd_plantBean.js
 更新时间：2021-04-9
 活动入口：京东APP我的-更多工具-种豆得豆
 已支持IOS京东多账号,云端多京东账号
@@ -9,19 +9,14 @@
 每个京东账号每天只能帮助3个人。多出的助力码将会助力失败。
 =====================================Quantumult X=================================
 [task_local]
-1 7-21/2 * * * https://gitee.com/lxk0301/jd_scripts/raw/master/jd_plantBean.js, tag=种豆得豆, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jdzd.png, enabled=true
-
+1 7-21/2 * * * jd_plantBean.js, tag=种豆得豆, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jdzd.png, enabled=true
 =====================================Loon================================
 [Script]
-cron "1 7-21/2 * * *" script-path=https://gitee.com/lxk0301/jd_scripts/raw/master/jd_plantBean.js,tag=京东种豆得豆
-
+cron "1 7-21/2 * * *" script-path=jd_plantBean.js,tag=京东种豆得豆
 ======================================Surge==========================
-京东种豆得豆 = type=cron,cronexp="1 7-21/2 * * *",wake-system=1,timeout=3600,script-path=https://gitee.com/lxk0301/jd_scripts/raw/master/jd_plantBean.js
-
+京东种豆得豆 = type=cron,cronexp="1 7-21/2 * * *",wake-system=1,timeout=3600,script-path=jd_plantBean.js
 ====================================小火箭=============================
-京东种豆得豆 = type=cron,script-path=https://gitee.com/lxk0301/jd_scripts/raw/master/jd_plantBean.js, cronexpr="1 7-21/2 * * *", timeout=3600, enable=true
-
-搬的https://github.com/uniqueque/QuantumultX/blob/4c1572d93d4d4f883f483f907120a75d925a693e/Script/jd_plantBean.js
+京东种豆得豆 = type=cron,script-path=jd_plantBean.js, cronexpr="1 7-21/2 * * *", timeout=3600, enable=true
 */
 const jd_heplers = require("./utils/JDHelpers.js");
 const jd_env = require("./utils/JDEnv.js");
@@ -45,17 +40,15 @@ const JD_API_HOST = "https://api.m.jd.com/client.action";
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
 let shareCodes = [
   // IOS本地脚本用户这个列表填入你要助力的好友的shareCode
-  //账号一的好友shareCode,不同好友的shareCode中间用@符号隔开
-  "eak7zp6drtizarovdmbpfyzrjewog6pafm6mfay@eikydcsb6qgb6vzcnpbvuhj4yqwpxjpkrwsqthi@4vvbjlml6tdfcm56dymyhay2ia33mplmmrmkwqi@mlrdw3aw26j3wdr2okwjer3xkbjcp6m6ndftywy@urahfsfiinkvoaazloezfalirm@olmijoxgmjutz2lzuiqhlaetagxozsuyibzao3q@olmijoxgmjutznhkcrgg3urla2gpgef5qscsssa@mlrdw3aw26j3wfyostskcnx3sude4ojhx5ze6za@e7lhibzb3zek2eoqjepbpvk33lvmhp2utkjhkay@olmijoxgmjutyfzhxyytbpa4dkc2orwd4vxdpjq@mlrdw3aw26j3wrfphqduo7dfgb7r2wiyssomtay@olmijoxgmjutysos2rgfzcdcknfjv6yiurhqbmy@mbwrj6cvqylyvwvsugi2e6m7ue",
-  //账号二的好友shareCode,不同好友的shareCode中间用@符号隔开
-  "eak7zp6drtizarovdmbpfyzrjewog6pafm6mfay@eikydcsb6qgb6vzcnpbvuhj4yqwpxjpkrwsqthi@4vvbjlml6tdfcm56dymyhay2ia33mplmmrmkwqi@mlrdw3aw26j3wdr2okwjer3xkbjcp6m6ndftywy@urahfsfiinkvoaazloezfalirm@olmijoxgmjutz2lzuiqhlaetagxozsuyibzao3q@olmijoxgmjutznhkcrgg3urla2gpgef5qscsssa@mlrdw3aw26j3wfyostskcnx3sude4ojhx5ze6za@e7lhibzb3zek2eoqjepbpvk33lvmhp2utkjhkay@olmijoxgmjutyfzhxyytbpa4dkc2orwd4vxdpjq@mlrdw3aw26j3wrfphqduo7dfgb7r2wiyssomtay@olmijoxgmjutysos2rgfzcdcknfjv6yiurhqbmy@mbwrj6cvqylyvwvsugi2e6m7ue",
+  "",
 ];
 let allMessage = ``;
 let currentRoundId = null; //本期活动id
 let lastRoundId = null; //上期id
 let roundList = [];
 let awardState = ""; //上期活动的京豆是否收取
-let randomCount = 0;
+let randomCount = $.isNode() ? 20 : 5;
+let num;
 !(async () => {
   await requireConfig();
   if (!cookiesArr[0]) {
@@ -104,19 +97,25 @@ async function jdPlantBean() {
   try {
     console.log(`获取任务及基本信息`);
     await plantBeanIndex();
+    for (let i = 0; i < $.plantBeanIndexResult.data.roundList.length; i++) {
+      if ($.plantBeanIndexResult.data.roundList[i].roundState === "2") {
+        num = i;
+        break;
+      }
+    }
     // console.log(plantBeanIndexResult.data.taskList);
     if ($.plantBeanIndexResult && $.plantBeanIndexResult.code === "0" && $.plantBeanIndexResult.data) {
       const shareUrl = $.plantBeanIndexResult.data.jwordShareInfo.shareUrl;
       $.myPlantUuid = getParam(shareUrl, "plantUuid");
       console.log(`\n【京东账号${$.index}（${$.UserName}）的${$.name}好友互助码】${$.myPlantUuid}\n`);
       roundList = $.plantBeanIndexResult.data.roundList;
-      currentRoundId = roundList[1].roundId; //本期的roundId
-      lastRoundId = roundList[0].roundId; //上期的roundId
-      awardState = roundList[0].awardState;
+      currentRoundId = roundList[num].roundId; //本期的roundId
+      lastRoundId = roundList[num - 1].roundId; //上期的roundId
+      awardState = roundList[num - 1].awardState;
       $.taskList = $.plantBeanIndexResult.data.taskList;
       subTitle = `【京东昵称】${$.plantBeanIndexResult.data.plantUserInfo.plantNickName}`;
-      message += `【上期时间】${roundList[0].dateDesc.replace("上期 ", "")}\n`;
-      message += `【上期成长值】${roundList[0].growth}\n`;
+      message += `【上期时间】${roundList[num - 1].dateDesc.replace("上期 ", "")}\n`;
+      message += `【上期成长值】${roundList[num - 1].growth}\n`;
       await receiveNutrients(); //定时领取营养液
       await doHelp(); //助力
       await doTask(); //做日常任务
@@ -140,7 +139,7 @@ async function doGetReward() {
   console.log(`【上轮京豆】${awardState === "4" ? "采摘中" : awardState === "5" ? "可收获了" : "已领取"}`);
   if (awardState === "4") {
     //京豆采摘中...
-    message += `【上期状态】${roundList[0].tipBeanEndTitle}\n`;
+    message += `【上期状态】${roundList[num - 1].tipBeanEndTitle}\n`;
   } else if (awardState === "5") {
     //收获
     await getReward();
@@ -158,18 +157,18 @@ async function doGetReward() {
     }
   } else if (awardState === "6") {
     //京豆已领取
-    message += `【上期兑换京豆】${roundList[0].awardBeans}个\n`;
+    message += `【上期兑换京豆】${roundList[num - 1].awardBeans}个\n`;
   }
-  if (roundList[1].dateDesc.indexOf("本期 ") > -1) {
-    roundList[1].dateDesc = roundList[1].dateDesc.substr(roundList[1].dateDesc.indexOf("本期 ") + 3, roundList[1].dateDesc.length);
+  if (roundList[num].dateDesc.indexOf("本期 ") > -1) {
+    roundList[num].dateDesc = roundList[num].dateDesc.substr(roundList[num].dateDesc.indexOf("本期 ") + 3, roundList[num].dateDesc.length);
   }
-  message += `【本期时间】${roundList[1].dateDesc}\n`;
-  message += `【本期成长值】${roundList[1].growth}\n`;
+  message += `【本期时间】${roundList[num].dateDesc}\n`;
+  message += `【本期成长值】${roundList[num].growth}\n`;
 }
 async function doCultureBean() {
   await plantBeanIndex();
   if ($.plantBeanIndexResult && $.plantBeanIndexResult.code === "0") {
-    const plantBeanRound = $.plantBeanIndexResult.data.roundList[1];
+    const plantBeanRound = $.plantBeanIndexResult.data.roundList[num];
     if (plantBeanRound.roundState === "2") {
       //收取营养液
       if (plantBeanRound.bubbleInfos && plantBeanRound.bubbleInfos.length) console.log(`开始收取营养液`);
@@ -549,29 +548,6 @@ async function helpShare(plantUuid) {
 async function plantBeanIndex() {
   $.plantBeanIndexResult = await request("plantBeanIndex"); //plantBeanIndexBody
 }
-function readShareCode() {
-  return new Promise(async (resolve) => {
-    $.get({ url: `http://jd.turinglabs.net/api/v2/jd/bean/read/${randomCount}/`, timeout: 10000 }, (err, resp, data) => {
-      try {
-        if (err) {
-          console.log(`${JSON.stringify(err)}`);
-          console.log(`${$.name} API请求失败，请检查网路重试`);
-        } else {
-          if (data) {
-            console.log(`随机取个${randomCount}码放到您固定的互助码后面(不影响已有固定互助)`);
-            data = JSON.parse(data);
-          }
-        }
-      } catch (e) {
-        $.logErr(e, resp);
-      } finally {
-        resolve(data);
-      }
-    });
-    await $.wait(15000);
-    resolve();
-  });
-}
 //格式化助力码
 function shareCodesFormat() {
   return new Promise(async (resolve) => {
@@ -583,10 +559,6 @@ function shareCodesFormat() {
       console.log(`由于您第${$.index}个京东账号未提供shareCode,将采纳本脚本自带的助力码\n`);
       const tempIndex = $.index > shareCodes.length ? shareCodes.length - 1 : $.index - 1;
       newShareCodes = shareCodes[tempIndex].split("@");
-    }
-    const readShareCodeRes = await readShareCode();
-    if (readShareCodeRes && readShareCodeRes.code === 200) {
-      newShareCodes = [...new Set([...newShareCodes, ...(readShareCodeRes.data || [])])];
     }
     console.log(`第${$.index}个京东账号将要助力的好友${JSON.stringify(newShareCodes)}`);
     resolve();
