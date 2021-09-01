265ce46c2f87976378396cc5ed57c875d98f2004
dongdongGit <1697919363@qq.com>
2021-09-01 23:26:46 +0800

    update jd_health

--- jd_health.js
+++ jd_health.js
@@ -204,7 +204,7 @@ async function getCommodities() {
           if (beans.length !== 0) {
             for (let key of Object.keys(beans)) {
               let vo = beans[key];
-              if (vo.title === reward && $.score >= vo.exchangePoints) {
+              if (vo.title == reward && $.score >= vo.exchangePoints) {
                 await $.wait(1000);
                 await exchange(vo.type, vo.id);
               }
