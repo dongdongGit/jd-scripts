c6444aae00df68c0a266d4a9c032ff5e465aa6ff
dongdong <dongdong@elemenx.com>
2021-09-08 09:41:12 +0800

    update

--- docker/proc_file.sh
+++ docker/proc_file.sh
@@ -27,14 +27,14 @@ else
 fi
 
 echo "处理jd_cfd_loop任务。。。"
-if [ ! $CRZAY_JOY_COIN_ENABLE ]; then
+if [ ! $JD_CFD_LOOP_ENABLE ]; then
    echo "默认启用jd_cfd_loop杀掉jd_cfd_loop任务，并重启"
    eval $(ps -ef | grep "jd_cfd_loop" | grep -v "grep" | awk '{print "kill "$1}')
    echo '' >/scripts/logs/jd_cfd_loop.log
    $CMD /scripts/jd_cfd_loop.js | ts >>/scripts/logs/jd_cfd_loop.log 2>&1 &
    echo "默认jd_cfd_loop重启完成"
 else
-   if [ $CRZAY_JOY_COIN_ENABLE = "Y" ]; then
+   if [ $JD_CFD_LOOP_ENABLE = "Y" ]; then
       echo "配置启用jd_cfd_loop，杀掉jd_cfd_loop任务，并重启"
       eval $(ps -ef | grep "jd_cfd_loop" | grep -v "grep" | awk '{print "kill "$1}')
       echo '' >/scripts/logs/jd_cfd_loop.log
