a73d6d3090f4308c383c01a636edb5b53455ab68
dongdong <dongdong@elemenx.com>
2021-09-17 08:38:32 +0800

    update jd_priceProtect

--- jd_priceProtect.js
+++ jd_priceProtect.js
@@ -1,15 +1,14 @@
 /*
-cron 5 2 * * *
-update 2021/7/25
-京东价格保护：脚本更新地址 https://raw.githubusercontent.com/LingFeng0918/jd_scripts/master/jd_priceProtect.js
+cron 35 0 * * * jd_priceProtect.js
+京东价格保护：脚本更新地址 https://raw.githubusercontent.com/yongyuanlin/jd_scripts/master//jd_priceProtect.js
 脚本兼容: QuantumultX, Node.js
 ==========================Quantumultx=========================
 打开手机客户端，或者浏览器访问 https://msitepp-fm.jd.com/rest/priceprophone/priceProPhoneMenu
 [rewrite_local]
-https:\/\/api\.m.jd.com\/api\?appid=siteppM&functionId=siteppM_priceskusPull url script-request-body https://raw.githubusercontent.com/LingFeng0918/jd_scripts/master/jd_priceProtectRewrite.js
+https:\/\/api\.m.jd.com\/api\?appid=siteppM&functionId=siteppM_priceskusPull url script-request-body https://raw.githubusercontent.com/yongyuanlin/jd_scripts/master/jd_priceProtect.js
 [task_local]
 # 京东价格保护
-5 0 * * * https://raw.githubusercontent.com/LingFeng0918/jd_scripts/master/jd_priceProtect.js, tag=京东价格保护, img-url=https://raw.githubusercontent.com/ZCY01/img/master/pricev1.png, enabled=true
+5 1 * * * https://raw.githubusercontent.com/yongyuanlin/jd_scripts/master/jd_priceProtect.js, tag=京东价格保护, img-url=https://raw.githubusercontent.com/ZCY01/img/master/pricev1.png, enabled=true
 */
 
 const jd_env = require('./utils/JDEnv.js');
@@ -147,6 +146,53 @@ function checkOnceAppliedResult() {
     });
   });
 }
+
+function totalBean() {
+  return new Promise(async (resolve) => {
+    const options = {
+      url: `https://wq.jd.com/user/info/QueryJDUserInfo?sceneval=2`,
+      headers: {
+        Accept: 'application/json,text/plain, */*',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'zh-cn',
+        Connection: 'keep-alive',
+        Cookie: $.cookie,
+        Referer: 'https://wqs.jd.com/my/jingdou/my.shtml?sceneval=2',
+        'User-Agent': 'jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1',
+      },
+      timeout: 10000,
+    };
+    $.post(options, (err, resp, data) => {
+      try {
+        if (err) {
+          console.log(`${JSON.stringify(err)}`);
+          console.log(`${$.name} API请求失败，请检查网路重试`);
+        } else {
+          if (data) {
+            data = JSON.parse(data);
+            if (data['retcode'] === 13) {
+              $.isLogin = false; //cookie过期
+              return;
+            }
+            if (data['retcode'] === 0) {
+              $.nickName = (data['base'] && data['base'].nickname) || $.UserName;
+            } else {
+              $.nickName = $.UserName;
+            }
+          } else {
+            console.log(`X东服务器返回空数据`);
+          }
+        }
+      } catch (e) {
+        $.logErr(e, resp);
+      } finally {
+        resolve();
+      }
+    });
+  });
+}
+
 function taskurl(functionid, body) {
   const urlStr = `${unifiedGatewayName}/api?appid=siteppM&functionId=${functionid}&forcebot=${$.HyperParam.forcebot}&t=${new Date().getTime()}`;
   return {
