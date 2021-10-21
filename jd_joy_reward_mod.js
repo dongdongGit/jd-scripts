9a214c5a6ec800477cb81467133443da1a6003f0
dongdong <dongdong@elemenx.com>
2021-10-21 13:36:36 +0800

    update jd_joy invoke key and jd_jxmc

--- jd_joy_reward_mod.js
+++ jd_joy_reward_mod.js
@@ -2,7 +2,7 @@
 cron "58 7,15,23 * * *" jd_joy_reward_Mod.js
  */
 //Mod by ccwav，20211014
-// prettier-ignore
+const config = require('./utils/config.js');
 const jd_helpers = require('./utils/JDHelpers.js');
 const jd_env = require('./utils/JDEnv.js');
 const $ = jd_env.env('宠汪汪积分兑换有就换版');
@@ -51,13 +51,13 @@ Date.prototype.Format = function (fmt) {
   }
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
-      cookie = cookiesArr[i];
+      $.cookie = cookie = cookiesArr[i];
       $.UserName = decodeURIComponent(cookie.match(/pt_pin=([^; ]+)(?=;?)/) && cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1]);
       $.index = i + 1;
       $.isLogin = true;
       $.nickName = '' || $.UserName;
 
-      await TotalBean();
+      await $.totalBean();
       console.log(`\n*****开始【京东账号${$.index}】${$.nickName || $.UserName}****\n`);
       if (!$.isLogin) {
         $.msg($.name, `【提示】cookie已失效`, `京东账号${$.index} ${$.nickName || $.UserName}\n请重新登录获取\nhttps://bean.m.jd.com/bean/signIndex.action`, {
@@ -308,7 +308,7 @@ async function joyReward() {
 }
 function getExchangeRewards() {
   let opt = {
-    url: '//jdjoy.jd.com/common/gift/getBeanConfigs?reqSource=h5&invokeKey=JL1VTNRadM68cIMQ',
+    url: `//jdjoy.jd.com/common/gift/getBeanConfigs?reqSource=h5&invokeKey=${config.invokeKey}`,
     method: 'GET',
     data: {},
     credentials: 'include',
@@ -318,7 +318,7 @@ function getExchangeRewards() {
   };
   return new Promise((resolve) => {
     let lkt = new Date().getTime();
-    let lks = $.md5('' + 'JL1VTNRadM68cIMQ' + lkt).toString();
+    let lks = $.md5(config.invokeKey + lkt).toString();
     const option = {
       url: 'https:' + taroRequest(opt)['url'] + $.validate,
       headers: {
@@ -370,7 +370,7 @@ function exchange(saleInfoId, orderSource) {
     deviceInfo: {},
   };
   let opt = {
-    url: '//jdjoy.jd.com/common/gift/new/exchange?reqSource=h5&invokeKey=JL1VTNRadM68cIMQ',
+    url: `//jdjoy.jd.com/common/gift/new/exchange?reqSource=h5&invokeKey=${config.invokeKey}`,
     data: body,
     credentials: 'include',
     method: 'POST',
@@ -380,7 +380,7 @@ function exchange(saleInfoId, orderSource) {
   };
   return new Promise((resolve) => {
     let lkt = new Date().getTime();
-    let lks = $.md5('' + 'JL1VTNRadM68cIMQ' + lkt).toString();
+    let lks = $.md5(config.invokeKey + lkt).toString();
     const option = {
       url: 'https:' + taroRequest(opt)['url'] + $.validate,
       body: `${JSON.stringify(body)}`,
@@ -427,59 +427,8 @@ function exchange(saleInfoId, orderSource) {
     });
   });
 }
-function TotalBean() {
-  return new Promise(async (resolve) => {
-    const options = {
-      url: `https://wq.jd.com/user/info/QueryJDUserInfo?sceneval=2`,
-      headers: {
-        Accept: 'application/json,text/plain, */*',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'zh-cn',
-        Connection: 'keep-alive',
-        Cookie: cookie,
-        Referer: 'https://wqs.jd.com/my/jingdou/my.shtml?sceneval=2',
-        'User-Agent': $.isNode()
-          ? process.env.JD_USER_AGENT
-            ? process.env.JD_USER_AGENT
-            : require('./USER_AGENTS').USER_AGENT
-          : $.getdata('JDUA')
-          ? $.getdata('JDUA')
-          : 'jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1',
-      },
-    };
-    $.post(options, (err, resp, data) => {
-      try {
-        if (err) {
-          console.log(`${JSON.stringify(err)}`);
-          console.log(`${$.name} API请求失败，请检查网路重试`);
-        } else {
-          if (data) {
-            data = JSON.parse(data);
-            if (data['retcode'] === 13) {
-              $.isLogin = false; //cookie过期
-              return;
-            }
-            if (data['retcode'] === 0) {
-              $.nickName = (data['base'] && data['base'].nickname) || $.UserName;
-            } else {
-              $.nickName = $.UserName;
-            }
-          } else {
-            console.log(`京东服务器返回空数据`);
-          }
-        }
-      } catch (e) {
-        $.logErr(e, resp);
-      } finally {
-        resolve();
-      }
-    });
-  });
-}
 function getJDServerTime() {
   return new Promise((resolve) => {
-    // console.log(Date.now())
     $.get(
       {
         url: 'https://a.jd.com//ajax/queryServerData.html',
